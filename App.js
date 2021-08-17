3a7037965c90bb44a8c12f6620132d15b20e5f24
Prince Vishwakarma <princevworld@gmail.com>
2021-08-17 16:57:00 +0530

    build

--- App.js
+++ App.js
@@ -1,21 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState, useRef } from "react";
+import {
+  Platform,
+  BackHandler,
+  Dimensions,
+  SafeAreaView,
+  View,
+  Image,Text
+} from "react-native";
+import { WebView } from "react-native-webview";
+import Constants from "expo-constants";
+import NetInfo from "@react-native-community/netinfo";
 
-export default function App() {
+const BACKGROUND_COLOR = "#FFFFFF";
+const DEVICE_WIDTH = Dimensions.get("window").width;
+const DEVICE_HEIGHT = Dimensions.get("window").height;
+const ANDROID_BAR_HEIGHT = Platform.OS === "android" ? Constants.statusBarHeight : 0;
+
+export default function App(props) {
+
+  const WEBVIEW = useRef()
+
+  const [loading, setLoading] = useState(true)
+  const [backButtonEnabled, setBackButtonEnabled] = useState(false)
+  const [isConnected, setConnected] = useState(true)
+
+  // Webview content loaded
+  function webViewLoaded() {
+    setLoading(false)
+  };
+
+  // Webview navigation state change
+  function onNavigationStateChange(navState) {
+    setBackButtonEnabled(navState.canGoBack)
+  };
+
+  useEffect(() => {
+    // Handle back event
+    function backHandler() {
+      if (backButtonEnabled) {
+        WEBVIEW.current.goBack();
+        return true;
+      }
+    };
+
+    // Subscribe to back state vent
+    BackHandler.addEventListener("hardwareBackPress", backHandler);
+
+    // Unsubscribe
+    return () => BackHandler.removeEventListener("hardwareBackPress", backHandler);
+  }, [backButtonEnabled])
+
+  useEffect(() => {
+    // Subscribe for net state
+    const netInfroSubscribe = NetInfo.addEventListener((state) => {
+      setConnected(state.isConnected)
+      if (!state.isConnected) {
+        alert("No Internet Connection");
+      }
+    });
+
+    // Clean up
+    return netInfroSubscribe
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: BACKGROUND_COLOR,
+      }}
+    >
+      <View
+        style={{
+          height: ANDROID_BAR_HEIGHT,
+          backgroundColor: BACKGROUND_COLOR,
+        }}
+      ></View>
+      {(loading || !isConnected) && (
+        <View
+          style={{
+            backgroundColor: BACKGROUND_COLOR,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: 10,
+            width: DEVICE_WIDTH,
+            height: DEVICE_HEIGHT + ANDROID_BAR_HEIGHT,
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+      
+          }}
+        >
+          <Image source={require("./assets/flash2.gif")}></Image>
+        </View>
+      )}
+      {isConnected && (
+        <WebView
+          onLoad={webViewLoaded}
+          ref={WEBVIEW}
+          useWebKit={true}
+          onNavigationStateChange={onNavigationStateChange}
+          source={{ uri: "https://roommate2.herokuapp.com/" }}
+        />
+      )}
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
